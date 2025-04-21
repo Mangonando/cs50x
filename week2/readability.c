5698d239ab5c3b1cb851b4906ac26d7cbb6748fa
Mangonando <juan.f.ramirezv@gmail.com>
2025-04-21 22:07:21 +0200

    Add count sentences

--- week2/readability.c
+++ week2/readability.c
@@ -12,42 +12,63 @@ int main(void)
 {
     //Input text
     string text = get_string("Text: ");
-    printf("%s\n", text);
+    printf("Text: %s\n", text);
 
     // Count the number of letters
     int letters = count_letters(text);
-    printf("%i\n", letters);
+    printf("Letters: %i\n", letters);
     // Count the number of words
     int words = count_words(text);
+    printf("Words: %i\n", words);
     // Count the sentences
     int sentences = count_sentences(text);
+    printf("Sentences: %i\n", sentences);
 }
 
 int count_letters (string text)
 {
     // Variable to storage the count of the words
-    int count = 0;
+    int count_letters = 0;
     // Loop the string aka sentence
     for (int i = 0; text[i] != '\0'; i++)
     {
         if (isalpha(text[i]))
         {
-            count ++;
+            count_letters ++;
         }
     }
     // Return number of letter in the text
-    return count;
+    return count_letters;
 }
 
 int count_words (string text)
 {
-    // Return number of words in text
-    return 0;
+    // Variable to storage the count of the words
+    int count_words = 0;
+    // Count number of words in sentence
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == ' ')
+        {
+            count_words ++;
+        }
+    }
+    // Return number of words in text. Count spaces + 1
+    return count_words + 1;
 }
 
 int count_sentences (string text)
 {
-    // Return number of sentence in text
-    // A sentence is any sequence of characters that ends with a . or a ! or a ? to be a sentence
-    return 0;
+    // Variable to storage the count of the sentences
+    int count_sentences = 0;
+    // Count number of sentences in text
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count_sentences ++;
+        }
+    }
+    // Return number of sentences in text. A sentence is any sequence of characters that ends with a . or a ! or a ? to be a sentence
+    return count_sentences;
 }
