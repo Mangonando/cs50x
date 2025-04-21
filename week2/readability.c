b54ca298c917e2cab220aa45942e59020c26ea3d
Mangonando <juan.f.ramirezv@gmail.com>
2025-04-21 22:42:44 +0200

    Add sentences counter and index

--- week2/readability.c
+++ week2/readability.c
@@ -17,18 +17,41 @@ int main(void)
     // Count the number of letters
     int letters = count_letters(text);
     printf("Letters: %i\n", letters);
+
     // Count the number of words
     int words = count_words(text);
     printf("Words: %i\n", words);
+
     // Count the sentences
     int sentences = count_sentences(text);
     printf("Sentences: %i\n", sentences);
+
+    // Compute the Coleman-Liau index
+    // L is the average number of letters per 100 words in the text
+    int L = (letters / words) * 100;
+    // S is the average number of sentences per 100 words in the text
+    int S = (sentences / words) * 100;
+    int index = 0.0588 * L - 0.296 * S - 15.8;
+    // Print the grade level
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
 
 int count_letters (string text)
 {
     // Variable to storage the count of the words
     int count_letters = 0;
+
     // Loop the string aka sentence
     for (int i = 0; text[i] != '\0'; i++)
     {
@@ -37,6 +60,7 @@ int count_letters (string text)
             count_letters ++;
         }
     }
+
     // Return number of letter in the text
     return count_letters;
 }
@@ -45,6 +69,7 @@ int count_words (string text)
 {
     // Variable to storage the count of the words
     int count_words = 0;
+
     // Count number of words in sentence
     for (int i = 0; text[i] != '\0'; i++)
     {
@@ -53,6 +78,7 @@ int count_words (string text)
             count_words ++;
         }
     }
+
     // Return number of words in text. Count spaces + 1
     return count_words + 1;
 }
@@ -61,6 +87,7 @@ int count_sentences (string text)
 {
     // Variable to storage the count of the sentences
     int count_sentences = 0;
+
     // Count number of sentences in text
     for (int i = 0; text[i] != '\0'; i++)
     {
@@ -69,6 +96,7 @@ int count_sentences (string text)
             count_sentences ++;
         }
     }
+
     // Return number of sentences in text. A sentence is any sequence of characters that ends with a . or a ! or a ? to be a sentence
     return count_sentences;
 }
