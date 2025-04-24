209f24014efd85629baa536468658137306ae59b
Mangonando <juan.f.ramirezv@gmail.com>
2025-04-24 11:43:08 +0200

    Check if it is not digits

--- week2/caesar.c
+++ week2/caesar.c
@@ -1,5 +1,9 @@
+#include <ctype.h>
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+bool only_digits(string s);
 
 int main(int argc, string argv[])
 {
@@ -9,7 +13,12 @@ int main(int argc, string argv[])
     {
         printf("Usage: ./caesar key\n");
         return 1;
-    } else
+    } else if (!only_digits(argv[1]))
+    {
+        printf("not only digits\n");
+        return 1;
+    }
+    else
     {
         // Get the 0 good to go for the program
         printf("Key is: %s\n", argv[1]);
@@ -18,4 +27,19 @@ int main(int argc, string argv[])
     //Input plainText
     // string plainText = get_string("plaintext:  ");
     // printf("plainText: %s\n", plainText);
+}
+
+bool only_digits(string s)
+{
+    // loop the string. Use < instead of <= due to c asuming the last one is ./caesar
+    for (int i = 0; i < strlen(s); i++)
+    {
+        // check if it is not as digit. handle error first
+        if (!isdigit(s[i]))
+        {
+            return false;
+        }
+    }
+    // Otherwise return true(outside the loop)
+    return true;
 }
