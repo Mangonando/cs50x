7182813897276c84bf0d519ba9e49d74d0bdd2c0
Mangonando <juan.f.ramirezv@gmail.com>
2025-04-25 09:18:14 +0200

    Substitution week 2. Start with main function and pseudo code

---
+++ week2/substitution.c
@@ -0,0 +1,19 @@
+int main(int argc, string argv[])
+{
+    // Get key
+    // Catch errors first with an if condition(argc and argv[])
+
+    // Validate key
+    // Check key length 26 chars.
+    // Check non-alphabetical chars
+    // No repetition of chars(case insensitive)
+
+    // Get plaintext good old get_string
+
+    // Encipher
+    // Map and check which letter from the key should map the letter
+    // Keep case
+    // Leave non-alphabetical chars as it is
+
+    // Print ciphertext
+}
