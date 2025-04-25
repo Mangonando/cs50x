596ccc65945520bdef19a40775b73114a55dfa8b
Mangonando <juan.f.ramirezv@gmail.com>
2025-04-25 09:32:35 +0200

    Check for key and its length

--- week2/substitution.c
+++ week2/substitution.c
@@ -1,10 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
 int main(int argc, string argv[])
 {
     // Get key
+    // NQXPOMAFTRHLZGECYJIUWSKDVB
     // Catch errors first with an if condition(argc and argv[])
 
     // Validate key
     // Check key length 26 chars.
+    if (argc != 2 || strlen(argv[1]) != 26)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
     // Check non-alphabetical chars
     // No repetition of chars(case insensitive)
 
