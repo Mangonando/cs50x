cb240612e49f612838251581335d5e017aa7e386
Mangonando <juan.f.ramirezv@gmail.com>
2025-08-01 00:40:01 +0200

    filter less week 4 wip

---
+++ week4/filter-less/helpers.c
@@ -0,0 +1,42 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Take average of red, green, and blue
+            float avg = (image[i][j].rgbtRed
+                         + image[i][j].rgbtGreen
+                         + image[i][j].rgbtBlue) / 3.0;
+
+            // Update pixel values
+            BYTE gray = (BYTE) round(avg);
+            image[i][j].rgbtRed = gray;
+            image[i][j].rgbtGreen = gray;
+            image[i][j].rgbtBlue = gray;
+        }
+    }
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
