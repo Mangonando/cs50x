373d40053ae99fdcb04164f8b76885eeb759b373
Mangonando <juan.f.ramirezv@gmail.com>
2025-08-02 00:12:43 +0200

    week 4 filter less sepia

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -1,6 +1,19 @@
 #include "helpers.h"
 #include <math.h>
 
+// Helper. Cap value at 255
+
+#define MAX_COLOR 255
+
+static BYTE cap(int val)
+{
+    if (val > MAX_COLOR)
+    {
+        return MAX_COLOR;
+    }
+    return (BYTE) val;
+}
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -14,8 +27,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
                          + image[i][j].rgbtGreen
                          + image[i][j].rgbtBlue) / 3.0;
 
-            // Update pixel values
+            //Round the int
             BYTE gray = (BYTE) round(avg);
+
+            // Update pixel values
             image[i][j].rgbtRed = gray;
             image[i][j].rgbtGreen = gray;
             image[i][j].rgbtBlue = gray;
@@ -26,7 +41,27 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Keep original color values
+            BYTE originalRed = image[i][j].rgbtRed;
+            BYTE originalGreen = image[i][j].rgbtGreen;
+            BYTE originalBlue = image[i][j].rgbtBlue;
+
+            // Compute sepia values
+            int sepiaRed = (int) round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            int sepiaGreen = (int) round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = (int) round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+
+            // Update pixel with sepia values
+            image[i][j].rgbtRed = cap(sepiaRed);
+            image[i][j].rgbtGreen = cap(sepiaGreen);
+            image[i][j].rgbtBlue = cap(sepiaBlue);
+        }
+    }
 }
 
 // Reflect image horizontally
