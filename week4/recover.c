6cc76f637799d669ade749e26f67664ae8b7c4e5
Mangonando <juan.f.ramirezv@gmail.com>
2025-09-10 23:25:41 +0200

    week 4 recover

--- week4/recover.c
+++ week4/recover.c
@@ -1,5 +1,7 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,15 +14,62 @@ int main(int argc, char *argv[])
 
     // Open the memory card
     FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
+    {
+        //File was not opened properly
+        printf("Could not open the file properly %s.\n", argv[1]);
+        return 1;
+    }
+
+    // Create a buffer for a block of data
+    uint8_t buffer[512];
+
+    FILE *img = NULL;
+    int file_count = 0;
+    char filename[8];
 
     // While there's still data left to read from the memory card
-    // Read 512 bytes into a buffer
+    // fread returns 0 (which is effectively false), loop will end
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        // Create JPEGs from the data
+        bool start_of_jpeg =
+            buffer[0] == 0xff &&
+            buffer[1] == 0xd8 &&
+            buffer[2] == 0xff &&
+            (buffer[3] & 0xf0) == 0xe0;
 
-    // Create JPEGs from the data
-    // If start of new jpeg
-    // If jpeg
-    // Else
+        // If start of new jpeg
+        if (start_of_jpeg)
+        {
+            if (img != NULL)
+            {
+                fclose(img);
+                img = NULL;
+            }
 
-    // Else
-    // If already found jpeg
+            // Create new file name
+            sprintf(filename, "%03i.jpg", file_count);
+            // Open new jpeg
+            img = fopen(filename, "w");
+            if (img == NULL)
+            {
+                fclose(card);
+                return 1;
+            }
+            file_count++;
+        }
+        // If jpeg
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, 512, img);
+        }
+    }
+    // Clean opened file
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+    fclose(card);
+    return 0;
 }
